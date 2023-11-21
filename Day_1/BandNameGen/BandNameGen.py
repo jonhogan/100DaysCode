92f6a69a9d29e44dc7299e0a264811d75285b165
Jon <jon.hogan83@gmail.com>
2023-11-20 18:23:47 -0600

    Final Band Name Generator Push

--- Day_1/BandNameGen/BandNameGen.py
+++ Day_1/BandNameGen/BandNameGen.py
@@ -1,1 +1,6 @@
 # Small program to generate a band name
+
+city = input("What city are you from? ")
+animal = input("What is your favorite animal? ")
+
+print(f"Your band name could be {city} {animal}!")
---
+++ Day_1/README.md
@@ -0,0 +1,5 @@
+Day 1 of 100 Days of Code (Python)
+
+Variables
+Input
+Output
