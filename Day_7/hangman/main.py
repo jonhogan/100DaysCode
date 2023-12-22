63f9cdd89b25213508a7906c540404779f85b7fa
Jon <jon.hogan83@gmail.com>
2023-12-22 14:22:10 -0600

    Added hanged man art, adjusted win/lose conditions

--- Day_7/hangman/main.py
+++ Day_7/hangman/main.py
@@ -13,13 +13,73 @@ def select_word(words):
 def guess_word(game_word, guessed_letters):
     puzzle_string = ''
     for letter in game_word:
-            if letter in guessed_letters:
-                puzzle_string += letter
-            else:
-                puzzle_string += '_'
+        if letter in guessed_letters:
+            puzzle_string += letter
+        else:
+            puzzle_string += '_'
     return puzzle_string
 
+def hanged_man(count):
+    if count == 0:
+        print(' ____ ')
+        print(' |  | ')
+        print('    | ')
+        print('    | ')
+        print('    | ')
+        print(' ___|_')
+        print('/     \\')
+    elif count == 1:
+        print(' ____ ')
+        print(' |  | ')
+        print(' O  | ')
+        print('    | ')
+        print('    | ')
+        print(' ___|_')
+        print('/     \\')
+    elif count == 2:
+        print(' ____ ')
+        print(' |  | ')
+        print(' O  | ')
+        print(' |  | ')
+        print('    | ')
+        print(' ___|_')
+        print('/     \\')
+    
+    elif count == 3:
+        print(' ____ ')
+        print(' |  | ')
+        print(' O  | ')
+        print('/|  | ')
+        print('    | ')
+        print(' ___|_')
+        print('/     \\')
+    
+    elif count == 4:
+        print(' ____ ')
+        print(' |  | ')
+        print(' O  | ')
+        print('/|\ | ')
+        print('    | ')
+        print(' ___|_')
+        print('/     \\')
 
+    elif count == 5:
+        print(' ____ ')
+        print(' |  | ')
+        print(' O  | ')
+        print('/|\ | ')
+        print('/   | ')
+        print(' ___|_')
+        print('/     \\')
+
+    elif count == 6:
+        print(' ____ ')
+        print(' |  | ')
+        print(' O  | ')
+        print('/|\ | ')
+        print('/ \ | ')
+        print(' ___|_')
+        print('/     \\')
 
 
 play = True
@@ -39,10 +99,10 @@ while play:
     guess_count = 0
     correct_letters = []
 
-    #puzzle_string = ''
-    while not correct_guess:
-        clear()
+    clear()
+    hanged_man(guess_count)
 
+    while not correct_guess:
         print(guess_word(secret_word, correct_letters))
 
         player_guess = input("Guess a letter in the word:\n").lower()
@@ -52,17 +112,19 @@ while play:
             correct_letters.append(player_guess)
         else:
             print("Wrong")
-        
-        if guess_count > 14:
+            guess_count += 1
+
+        if guess_count > 5:
             correct_guess = True
 
         if guess_word(secret_word, correct_letters) == secret_word:
             correct_guess = True
             print(f"{secret_word}!!")
             print("You win!")
-        guess_count += 1
-
+        
         time.sleep(.5)
+        clear()
+        hanged_man(guess_count)
         puzzle_string = ''
 
     replay = input("Game over. would you like to replay? (y/n)\n").lower()
